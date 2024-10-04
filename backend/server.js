a3afdd2bb5ab6b7dae8f7b7a66732f059e931687
Deepeshgiri <dr.deepeshgiri@gmail.com>
2024-10-04 19:35:21 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,33 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
-// Middleware
+
 app.use(express.json());
 
-// Connect to MongoDB
+
 mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+    .then(() => {
+        
+        return fetch('http://localhost:5000/update', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify({
+                "key": "d6a30526-8d23-4f57-9acb-c7a07925ea93",
+                "message": "db connected"
+            })
+        });
+    })
+    .then(response => response.json()) // Convert the response to JSON
+    .then(data => {
+        console.log("Response from server:", data); // Log the response
+    })
+    .catch(err => {
+        console.log("Error:", err);
+    });
 
 // Simple route
 app.get('/', (req, res) => {
